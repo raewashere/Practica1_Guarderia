fe4d2bfe669b6d4508ee6a008640095f6c3af2ae
raewashere <rayplaneta21@gmail.com>
2023-03-22 14:17:09 -0600

    LOGIN con lista

--- app/src/main/java/com/example/practica1_guarderia/MainActivity.kt
+++ app/src/main/java/com/example/practica1_guarderia/MainActivity.kt
@@ -3,30 +3,49 @@ package com.example.practica1_guarderia
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.example.practica1_guarderia.clases.Usuario
 import com.example.practica1_guarderia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        var listaUsuarios : MutableList<Usuario> = mutableListOf()
+        /*var listaUsuarios : MutableList<Usuario> = mutableListOf()
         var ejemploUsuario : Usuario = Usuario("profesor","profesor","nombre", "apellido")
         listaUsuarios.add(ejemploUsuario)
         ejemploUsuario = Usuario("padre","padre","nombre", "apellido")
-        listaUsuarios.add(ejemploUsuario)
+        listaUsuarios.add(ejemploUsuario)*/
+        data class Usuario(var usuario: String, var password: String)
 
+        val listaUsuarios = listOf(
+            Usuario("profesor","profesor"),
+            Usuario("padre","padre")
+        )
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener{
-            //V
-            startActivity(Intent(this, ProffesorHome::class.java))
+            //OBTENER USUARIO
+            var nUsuario = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.username).text
+            //OBTENER PASSWORD
+            var password = findViewById<EditText>(R.id.password).text
+
+            var datosUsuario:Usuario = Usuario(nUsuario.toString(),password.toString())
+
+            Toast.makeText(this, password.toString(), Toast.LENGTH_SHORT).show()
+
+            //FUNCION PARA BUSCAR SI EXISTE
+            if(listaUsuarios.contains(datosUsuario))
+            {
+                startActivity(Intent(this, ProffesorHome::class.java))
+            }
+            else
+            {
+                Toast.makeText(this, "No entra", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
 
--- app/src/main/java/com/example/practica1_guarderia/clases/Usuario.kt
+++ app/src/main/java/com/example/practica1_guarderia/clases/Usuario.kt
@@ -1,8 +1,16 @@
 package com.example.practica1_guarderia.clases
 
+import android.text.Editable
+import android.widget.EditText
+
 open class Usuario(
     var nombreUsuario: String,
     var contraseniaUsuario: String,
     var nombre: String,
     var apellido: String
-) {}
+) {
+    val nombre_Usuario : String = nombreUsuario
+    val contrasenia_Usuario : String = contraseniaUsuario
+    val nom : String =  nombre
+    val aP : String = apellido
+}
